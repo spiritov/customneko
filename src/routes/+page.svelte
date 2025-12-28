ac75cf22b0ad1dee1362d9a75dbf061918097f42
bailey <spiritov_v@pm.me>
2025-12-27 17:35:33 -0700

    build with external image source

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import CustomNeko from '$lib/components/CustomNeko.svelte';
-	import zigzagoon from '$lib/assets/zigzagoon.png';
 </script>
 
-<CustomNeko image_path={zigzagoon} sprite_width={40} sprite_height={40} />
+<CustomNeko
+	image_path={'https://files.catbox.moe/0fqbi1.png'}
+	sprite_width={40}
+	sprite_height={40}
+/>
